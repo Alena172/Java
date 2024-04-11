6f8786e04efc598d699c8ee000fe12023d3b8fd6
Alena172 <149298093+Alena172@users.noreply.github.com>
2024-04-11 10:53:40 +0300

    Add files via upload

--- task13/src/main/java/org/example/Task13Application.java
+++ task13/src/main/java/org/example/Task13Application.java
@@ -3,8 +3,10 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Task13Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Task13Application.class, args);
     }
