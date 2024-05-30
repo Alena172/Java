c216916f91e303a7f768a3eb57d4cb2fec074713
Alena172 <149298093+Alena172@users.noreply.github.com>
2024-05-31 01:59:06 +0300

    Add files via upload

---
+++ task22/src/main/java/com/example/ExecutionTimeLoggerAspect.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExecutionTimeLoggerAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExecutionTimeLoggerAspect.class);
+
+    @Around("execution(* com.example.*Service.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        logger.info("{} executed in  {} ms", joinPoint.getSignature(), executionTime);
+        return proceed;
+    }
+}
